20dc655bf289e1d92a588b5050b5fe31e25989ac
JavierQ <javierquidulef@tdf.edu.ar>
2022-07-27 22:59:09 -0300

    se agrego funcionalidad: el mayor de dos numeros al archivo llamado tercerArchivo.js

--- tercerArchivo.js
+++ tercerArchivo.js
@@ -1,1 +1,18 @@
 console.log("hola mundo");
+console.log("hola mundo ramaTercerArchivaJS");
+
+let numero1 = 13;
+let numero2 = 5;
+if (numero1 == numero2) {
+    console.log("Los numeros son iguales");
+} else if (numero1 > numero2) {
+    console.log("el numero mayor es " + numero1);
+} else {
+    console.log("el numero mayor es " + numero2);
+}
+
+/* Documento Js actualizado 
+   modificacion: se agrego el condicional if para comparar cual de dos numeros es mayor
+   fecha creacion: 27-07-2022
+   hora creacion: 22:50hs
+*/
